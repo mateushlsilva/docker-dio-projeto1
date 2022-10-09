062abde8d613c5c22f21ecdcd81a03d8a21a63c0
Mateus Silva <mateushenriquesjc203@hotmail.com>
2022-10-09 12:58:32 -0300

    update

--- client/script/script.js
+++ client/script/script.js
@@ -5,7 +5,7 @@ function cadastrar(){
     let email = document.getElementById('email').value
     let senha = document.getElementById('senha').value
 
-    axios.post("http://localhost:3001/users/cadastrar", 
+    axios.post("http://192.168.0.20:3001/users/cadastrar", 
         {name: nome,
         email: email,
         senha: senha} 
