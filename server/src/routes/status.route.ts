9e01a6b01afc2f4f78c0fb734eec35cab08f4fa0
Mateus Silva <mateushenriquesjc203@hotmail.com>
2022-10-09 12:39:58 -0300

    update

--- server/src/routes/status.route.ts
+++ server/src/routes/status.route.ts
@@ -2,7 +2,6 @@ import { Router, Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
 
 const statusRouter = Router()
-const historico = require('../models/historicTable')
 
 statusRouter.get('/status', (req: Request, res: Response, next: NextFunction)=>{
     res.sendStatus(StatusCodes.OK)
