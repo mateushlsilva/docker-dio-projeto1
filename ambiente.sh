bff3608522fa94ef85458a2342d1426dd7ee5882
Mateus Silva <mateushenriquesjc203@hotmail.com>
2022-10-10 09:54:05 -0300

    update ambiente

--- ambiente.sh
+++ ambiente.sh
@@ -2,10 +2,8 @@
 
 docker image build -t server:1.0 ./server
 
-mkdir -p /data/mysql
-
 docker-compose up -d
 
-echo "Inicializando o servidor por favor espere um pouco!";sleep(20)
+echo "Inicializando o servidor por favor espere um pouco!" && sleep 20
 
 docker run --name server -dti -p 3001:3001 server:1.0
